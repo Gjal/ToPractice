2c67bbda5e58dde05fb137a410457e887449e43c
Gjal <34637704+Gjal@users.noreply.github.com>
2019-05-05 22:33:57 +0800

    Create pivotIndex.java
    pivotIndex

---
+++ pivotIndex.java
@@ -0,0 +1,27 @@
+###寻找数组的中心索引###
+(''')
+public int pivotIndex(int[] nums) {
+        int sum=0;
+        int leftSum=0;
+        int rightSum=0;
+        
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            if(i==0){
+                leftSum=0;
+            }
+            else{
+                leftSum=leftSum+nums[i-1];
+            }
+            
+            rightSum=sum-leftSum-nums[i];
+            if(leftSum==rightSum){
+                return i;
+            }
+        }
+        return -1;
+    }
+(''')

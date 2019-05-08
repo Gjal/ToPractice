0b6037b9d33f0cf8ed677f172375dbc5aaca3c82
Gjal <447650472@qq.com>
2019-05-08 22:47:39 +0800

    Create dominantIndex.java
    747. 至少是其他数字两倍的最大数

---
+++ dominantIndex.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max=0;
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
+                index=i;
+            }
+        }
+        int half=max/2;
+        for(int i=0;i<nums.length;i++){
+            if(i==index) continue;
+            if(nums[i]<=half){
+                continue;
+            }else{
+                return -1;
+            }
+        }
+        return index;
+    }
+}

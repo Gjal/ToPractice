065485311770ddf7083622ae2465ae4f2a6ed90b
Gjal <447650472@qq.com>
2019-05-22 19:08:50 +0800

    Create DiagonalTraverse.java

---
+++ DiagonalTraverse.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class DiagonalTraverse{
+    public int[] findDiagonalOrder(int[][] matrix){
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int r=0,c=0;
+        int ans[]=new int[row*col];
+        for(int i=0;i<row*col;i++){
+            ans[i]=matrix[r][c];
+            if((r+c)%2==0){
+                if(r==0){
+                    c++;
+                }else if(c==col-1){
+                    r++;
+                }else{
+                    r--;
+                    c++;
+                }
+            }else{
+                if(r==0){
+                    r++;
+                }else if(r==row-1){
+                    c++;
+                }else{
+                    r++;
+                    c--;
+                }
+            }
+        }
+        return ans;
+    }
+}

ba388d72c9f1f62b54bc034de47921393f099493
Gjal <34637704+Gjal@users.noreply.github.com>
2019-06-15 02:29:32 +0000

    Create Hanoi.java
    汉诺塔游戏

---
+++ Hanoi.java
@@ -0,0 +1,11 @@
+public class Hanoi{
+public void Hannoi (int n, char a, char b, char c){
+if(n==1){
+    System.out.println(a+"-->"+c);
+}
+else{
+    method(n-1,a,c,b);
+    method(1,a,b,c);
+    method(n-1,b,a,c);
+}
+}

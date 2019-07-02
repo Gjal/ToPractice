a2631c91a15e9caf50d5c05ce5fbd99a86507409
Gjal <34637704+Gjal@users.noreply.github.com>
2019-07-02 19:47:09 +0800

    67.二进制求和

--- AddBinary.java
+++ AddBinary.java
@@ -5,7 +5,7 @@ public String addBinary(String a, String b) {
         for(int i=a.length()-1,j=b.length()-1;i>=0||j>=0;i--,j--){
             int sum=r;
             sum+=i>=0?a.charAt(i)-'0':0;
-            sum+=j>=0?b.charAt(j)-'0':0;
+            sum+=j>=0?b.charAt(j)-'0':0; 
             res.append(sum%2);
             r=sum/2;
         }

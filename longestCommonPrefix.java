55436595c6acd3cd1195b4027fafcaa5f3a1f66d
Gjal <34637704+Gjal@users.noreply.github.com>
2019-07-04 15:57:14 +0800

    14.最长公共前缀

--- longestCommonPrefix.java
+++ longestCommonPrefix.java
@@ -1,5 +1,5 @@
 //14.最长公共前缀
-//方法一.LSP(LSP(LSP(S1,S2),S3)...Sn)
+//方法一.LSP(LSP(LSP(S1,S2),S3)...Sn) 水平扫描法
 public String longestCommonPrefix(String[] strs) {
         String prefix=strs[0];
         for(int i=1;i<strs.length;i++){
@@ -10,7 +10,7 @@ public String longestCommonPrefix(String[] strs) {
         }
         return prefix;
     }
-//方法二.S1[0]-S2[0]-S3[0]....
+//方法二.S1[0]-S2[0]-S3[0]....     水平扫描法
 public String longestCommonPrefix(String[] strs) {
         for(int i=0;i<strs[0].length();i++){
             for(int j=1;j<strs.length;j++){
@@ -20,4 +20,27 @@ public String longestCommonPrefix(String[] strs) {
             }
         }
         return strs[0];
+    }
+//方法三.分治  
+ public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0||strs==null) return "";
+        return longestCommonPrefix(strs,0,strs.length-1);
+    }
+    private String longestCommonPrefix(String[] strs,int l,int r){
+        if(l==r)
+            return strs[l];
+        else{
+            int mid=(l+r)/2;
+            String lcpLeft=longestCommonPrefix(strs,l,mid);
+            String lcpRight=longestCommonPrefix(strs,mid+1,r);
+            return commonPrefix(lcpLeft,lcpRight);
+        }
+    }
+    private String commonPrefix(String s1,String s2){
+        int minLen=Math.min(s1.length(),s2.length());
+        for(int i=0;i<minLen;i++){
+            if(s1.charAt(i)!=s2.charAt(i))
+                return s1.substring(0,i);
+        }
+        return s1.substring(0,minLen);
     }

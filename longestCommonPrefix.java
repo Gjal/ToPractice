97e8f018c72848013801aa31d0f4107d4e7fcfaf
Gjal <34637704+Gjal@users.noreply.github.com>
2019-07-05 11:33:51 +0800

    14.最长公共前缀

--- longestCommonPrefix.java
+++ longestCommonPrefix.java
@@ -44,3 +44,28 @@ public String longestCommonPrefix(String[] strs) {
         }
         return s1.substring(0,minLen);
     }
+//方法四．二分查找法
+ public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0||strs==null) return "";
+        int minLen=Integer.MAX_VALUE;
+        for(String str:strs)
+            minLen=Math.min(str.length(),minLen);
+        int low=1;
+        int high=minLen;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(isCommonPrefix(strs,mid))
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return strs[0].substring(0,(low+high)/2);
+    }
+    private boolean isCommonPrefix(String[] strs,int len){
+        String str1=strs[0].substring(0,len);
+        for(int i=1;i<strs.length;i++){
+            if(!strs[i].startsWith(str1))
+                return false;
+        }
+        return true;
+    }

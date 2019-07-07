fbec9f65ec6022742074154ab9a83eadb4a2c855
Gjal <34637704+Gjal@users.noreply.github.com>
2019-07-07 10:57:13 +0800

    @167.两数之和

---
+++ twoSumII.java
@@ -0,0 +1,20 @@
+//167.两数之和
+public int[] twoSum(int[] numbers, int target) {
+       int i=0;
+        int j=numbers.length-1;
+        int[] res=new int[2];
+        int sum=0;
+        while(i<j){
+            sum=numbers[i]+numbers[j];
+            if(sum==target){
+                res[0]=i+1;
+                res[1]=j+1;
+                break;
+            }else if(sum>target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return res;
+     }

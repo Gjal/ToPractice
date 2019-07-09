1e996dce5725f7b4714de0af947d6f4ce0628309
Gjal <34637704+Gjal@users.noreply.github.com>
2019-07-09 14:37:31 +0800

    27.移除元素

---
+++ removeElement.java
@@ -0,0 +1,11 @@
+//27.移除元素
+public int removeElement(int[] nums, int val) {
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=val){
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        return i;
+    }

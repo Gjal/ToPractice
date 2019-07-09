dcbe753478c377c8d984118ec0e2af1d6da7974d
Gjal <34637704+Gjal@users.noreply.github.com>
2019-07-09 15:05:47 +0800

    27.移除元素

--- removeElement.java
+++ removeElement.java
@@ -1,4 +1,5 @@
 //27.移除元素
+//拷贝覆盖
 public int removeElement(int[] nums, int val) {
         int i=0;
         for(int j=0;j<nums.length;j++){
@@ -8,4 +9,15 @@ public int removeElement(int[] nums, int val) {
             }
         }
         return i;
+}
+//交换移除
+public int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==val){
+                nums[i]=nums[n-1];
+                n--;
+            }
+        }
+        return n;
     }

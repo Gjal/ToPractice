0e1de15c4a46eb2845389a39940fe99793197d2d
Gjal <34637704+Gjal@users.noreply.github.com>
2019-07-11 08:48:12 +0800

    485.最大连续1的个数

--- findMaxConsecutiveOnes.java
+++ findMaxConsecutiveOnes.java
@@ -1,4 +1,5 @@
  //485.最大连续1的个数
+//0
  public int findMaxConsecutiveOnes(int[] nums) {
         int res=0;
         int j=0;
@@ -11,6 +12,20 @@
             }
         }
         return (res>j)?res:j;
+    }
+//1
+public int findMaxConsecutiveOnes(int[] nums) {
+        int res=0;
+        int num=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                num++;
+                res=(res>num)?res:num;
+            }else{
+                num=0;
+            }
+        }
+        return res;
     }
 
 
